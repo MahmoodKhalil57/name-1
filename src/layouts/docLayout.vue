3c4f9a7fb680739a7121d83bb2466692f592f9a4
MahmoodKhalil57 <2393401k@student.gla.ac.uk>
2022-05-01 20:40:57 +0100

    Whatsapp button

--- src/layouts/docLayout.vue
+++ src/layouts/docLayout.vue
@@ -1,14 +1,13 @@
 <template>
-  <q-layout view="hHh lpR lFr">
+  <q-layout view="hhh lpr fff">
+    <q-header elevated>
+      <q-toolbar>
+        <q-avatar>
+          <img src="\SVG\twomangoes-ripe.svg" />
+        </q-avatar>
 
-    <q-header elevated class="bg-primary text-white q-pa-xl" height-hint="98">
-
-      <q-avatar>
-        <img src="/SVG/manggo.svg">
-      </q-avatar>
-      Title
-
-
+        <q-toolbar-title>Farmweb</q-toolbar-title>
+      </q-toolbar>
 
       <q-tabs alignment="center">
         <q-route-tab to="/page1" label="Page One" />
@@ -20,41 +19,71 @@
 
     <q-page-container>
       <router-view />
-    </q-page-container>
 
-    <q-footer class="transparent">
-      <div class="q-pa-md q-gutter-sm">
-        <q-btn push color="green" text-color="White" rounded>
+      <q-page-sticky
+        position="bottom-left"
+        :scroll-offset="10"
+        :offset="[18, 18]"
+      >
+        <q-btn push color="green" rounded>
           <q-avatar icon="fa-brands fa-whatsapp"> </q-avatar>
-          <q-item-section>
-            Whatsapp Us
-          </q-item-section>
+          Whatsapp Us
           <q-badge color="red" rounded floating>
             <q-avatar icon="fa-solid fa-exclamation" size="1.5em"> </q-avatar>
           </q-badge>
-
         </q-btn>
+      </q-page-sticky>
+    </q-page-container>
+
+    <q-footer>
+      <div class="q-pa-md q-gutter-sm">
+        <p>
+          I was gonna take the time to sit down and write you a little letter.
+          But I thought a song would probably be a little better. Instead of a
+          ,letter. That you'd probably just shred up--yeah. I stumbled on your
+          picture yesterday and it made me stop and think of. How much of a
+          waste it'd ,be for me to put some ink to, a stupid piece a. Paper, I'd
+          rather let you see how. Much I fucking hate you in a freestyle.kol
+          5ara
+        </p>
+        <p>
+          So bad, I’m so good that I’m so bad. Guarantee I’ll be the greatest
+          thing you ever had. ‘Cause you ain't never met nobody like me. And you
+          ain't gonna wanna fuck nobody else again. I'ma make you learn to
+          appreciate me, differentiate me. From these phony, little fishy and
+          sissy fake G’s. Skip over the "huggy bear," and all the "kissy face,"
+          please. Initiate phase three, missy, now service me. Take another shot
+          of Jäger, shakin' so nervously. Take your time, baby, oh, you’re the
+          bomb, baby. Oh, you’re doing that even better than your mom, lady. I
+          told you why I’m Shady, you didn't listen now, did ya?. Relax woman,
+          you know that I’m only kiddin' with ya. Got a twisted sense of humor,
+          its warped, but I didn't hit ya. I think you’re finally starting to
+          get the picture. I’m just… So bad, I’m so good that I’m so bad.
+          Guarantee I’ll be the greatest thing you ever had. ‘Cause you ain't
+          never met nobody like me. And you ain't gonna wanna fuck nobody else
+          again. I'ma make you learn to appreciate me, differentiate me. From
+          these phony, little fishy and sissy fake G’s. Skip over the.
+        </p>
       </div>
     </q-footer>
-
   </q-layout>
 </template>
 
 <script>
-import { ref } from 'vue'
+  import { ref } from 'vue'
 
-export default {
-  // name: 'LayoutName',
+  export default {
+    // name: 'LayoutName',
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+    // setup () {
+    //   const leftDrawerOpen = ref(false)
 
-    return {
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
+    //   return {
+    //     leftDrawerOpen,
+    //     toggleLeftDrawer () {
+    //       leftDrawerOpen.value = !leftDrawerOpen.value
+    //     }
+    //   }
+    // }
   }
-}
 </script>
--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,56 +1,125 @@
 <template>
   <q-page padding>
     <!-- content -->
-    <header>HEADER BTETSHAF YA BRO </header>
-    <h3> lorem ipsum </h3>
+    <h3>lorem ipsum</h3>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus
-      accumsan et viverra justo commodo. Proin sodales pulvinar tempor.
-      Cum sociis natoque penatibus et magnis dis parturient montes,
-      nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate,
-      felis tellus mollis orci, sed rhoncus sapien nunc eget odio.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus
-      accumsan et viverra justo commodo. Proin sodales pulvinar tempor.
-      Cum sociis natoque penatibus et magnis dis parturient montes,
-      nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate,
-      felis tellus mollis orci, sed rhoncus sapien nunc eget odio.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus
-      accumsan et viverra justo commodo. Proin sodales pulvinar tempor.
-      Cum sociis natoque penatibus et magnis dis parturient montes,
-      nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate,
-      felis tellus mollis orci, sed rhoncus sapien nunc eget odio.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus
-      accumsan et viverra justo commodo. Proin sodales pulvinar tempor.
-      Cum sociis natoque penatibus et magnis dis parturient montes,
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod
+      bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra
+      justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus
+      et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum,
+      nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus
+      sapien nunc eget odio. Lorem ipsum dolor sit amet, consectetur adipiscing
+      elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus
+      accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum
+      sociis natoque penatibus et magnis dis parturient montes, nascetur
+      ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis
+      tellus mollis orci, sed rhoncus sapien nunc eget odio. Lorem ipsum dolor
+      sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.
+      Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.
+      Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis
+      parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus
+      pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget
+      odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+      euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et
+      viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque
+      penatibus et magnis dis parturient montes,
     </p>
-    <br><br><br><br><br><br><br><br>
+    <br />
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus
-      accumsan et viverra justo commodo. Proin sodales pulvinar tempor.
-      Cum sociis natoque penatibus et magnis dis parturient montes,
-      nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate,
-      felis tellus mollis orci, sed rhoncus sapien nunc eget odio.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus
-      accumsan et viverra justo commodo. Proin sodales pulvinar tempor.
-      Cum sociis natoque penatibus et magnis dis parturient montes,
-      nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate,
-      felis tellus mollis orci, sed rhoncus sapien nunc eget odio.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus
-      accumsan et viverra justo commodo. Proin sodales pulvinar tempor.
-      Cum sociis natoque penatibus et magnis dis parturient montes,
-      nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate,
-      felis tellus mollis orci, sed rhoncus sapien nunc eget odio.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus
-      accumsan et viverra justo commodo. Proin sodales pulvinar tempor.
-      Cum sociis natoque penatibus et magnis dis parturient montes,
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod
+      bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra
+      justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus
+      et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum,
+      nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus
+      sapien nunc eget odio. Lorem ipsum dolor sit amet, consectetur adipiscing
+      elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus
+      accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum
+      sociis natoque penatibus et magnis dis parturient montes, nascetur
+      ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis
+      tellus mollis orci, sed rhoncus sapien nunc eget odio. Lorem ipsum dolor
+      sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.
+      Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.
+      Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis
+      parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus
+      pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget
+      odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+      euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et
+      viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque
+      penatibus et magnis dis parturient montes,
+    </p>
+    <p>
+      But I do know one thing though. Bitches, they come, they go. Saturday
+      through Sunday, Monday. Monday through Sunday, yo. Maybe I'll love you one
+      day. Maybe we'll someday grow. 'Til then just sit your drunk ass on that
+      fuckin' runway, ho. ‘Cause I can't be your Superman. Can't be your
+      Superman. Can't be your Superman. Can't be your Superman. I know you want
+      me, baby, I think I want you too. (I think I love you, baby) I think I
+      love you too. I'm here to save you, girl, come be in Shady's world. I
+      wanna grow together, let's let our love unfurl. You know you want me,
+      baby, you know I want you too. They call me Superman, I'm here to rescue
+      you. I wanna save you, girl, come be in Shady's world. (Ooh boy, you drive
+      me crazy) Bitch, you make me hurl. First thing you say: (I'm not fazed. I
+      hang around big stars all day. I don't see what the big deal is anyway.
+      You're just plain old Marshall to me). Ooh yeah, girl, run that game.
+      (Hailie Jade, I love that name. Love that tattoo, what's that say?. "Rot
+      In Pieces," uh, that's great). But I do know one thing though. Bitches,
+      they come, they go. Saturday through Sunday, Monday. Monday through
+      Sunday, yo. Maybe I'll love you one day. Maybe we'll someday grow. 'Til
+      then just sit your drunk ass on that fuckin' runway, ho. ‘Cause I can't be
+      your Superman. Can't be your Superman. Can't be your Superman. Can't be
+      your Superman. I know you want me, baby, I think I want you too. (I think
+      I love you, baby) I think I love you too. I'm here to save you, girl, come
+      be in Shady's world. I wanna grow together, let's let our love unfurl. You
+      know you want me, baby, you know I want you too. They call me Superman,
+      I'm here to rescue you. I wanna save you, girl, come be in Shady's world.
+      (Ooh boy, you drive me crazy) Bitch, you make me hurl. First thing you
+      say: (I'm not fazed. I hang around big stars all day. I don't see what the
+      big deal is anyway. You're just plain old Marshall to me). Ooh yeah, girl,
+      run that game. (Hailie Jade, I love that name. Love that tattoo, what's
+      that say?. "Rot In Pieces," uh, that's great). But I do know one thing
+      though. Bitches, they come, they go. Saturday through Sunday, Monday.
+      Monday through Sunday, yo. Maybe I'll love you one day. Maybe we'll
+      someday grow. 'Til then just sit your drunk ass on that fuckin' runway,
+      ho. ‘Cause I can't be your Superman. Can't be your Superman. Can't be your
+      Superman. Can't be your Superman. I know you want me, baby, I think I want
+      you too. (I think I love you, baby) I think I love you too. I'm here to
+      save you, girl, come be in Shady's world. I wanna grow together, let's let
+      our love unfurl. You know you want me, baby, you know I want you too. They
+      call me Superman, I'm here to rescue you. I wanna save you, girl, come be
+      in Shady's world. (Ooh boy, you drive me crazy) Bitch, you make me hurl.
+      First thing you say: (I'm not fazed. I hang around big stars all day. I
+      don't see what the big deal is anyway. You're just plain old Marshall to
+      me). Ooh yeah, girl, run that game. (Hailie Jade, I love that name. Love
+      that tattoo, what's that say?. "Rot In Pieces," uh, that's great). But I
+      do know one thing though. Bitches, they come, they go. Saturday through
+      Sunday, Monday. Monday through Sunday, yo. Maybe I'll love you one day.
+      Maybe we'll someday grow. 'Til then just sit your drunk ass on that
+      fuckin' runway, ho. ‘Cause I can't be your Superman. Can't be your
+      Superman. Can't be your Superman. Can't be your Superman. I know you want
+      me, baby, I think I want you too. (I think I love you, baby) I think I
+      love you too. I'm here to save you, girl, come be in Shady's world. I
+      wanna grow together, let's let our love unfurl. You know you want me,
+      baby, you know I want you too. They call me Superman, I'm here to rescue
+      you. I wanna save you, girl, come be in Shady's world. (Ooh boy, you drive
+      me crazy) Bitch, you make me hurl. First thing you say: (I'm not fazed. I
+      hang around big stars all day. I don't see what the big deal is anyway.
+      You're just plain old Marshall to me). Ooh yeah, girl, run that game.
+      (Hailie Jade, I love that name. Love that tattoo, what's that say?. "Rot
+      In Pieces," uh, that's great). But I do know one thing though. Bitches,
+      they come, they go. Saturday through Sunday, Monday. Monday through
+      Sunday, yo. Maybe I'll love you one day. Maybe we'll someday grow. 'Til
+      then just sit your drunk ass on that fuckin' runway, ho. ‘Cause I can't be
+      your Superman. Can't be your Superman. Can't be your Superman. Can't be
+      your Superman. I know you want me, baby, I think I want you too. (I think
+      I love you, baby) I think I love you too. I'm here to save you, girl, come
+      be in Shady's world. I wanna grow together, let's let our love unfurl. You
+      know you want me, baby, you know I want you too. They call me Superman,
+      I'm here to rescue you. I wanna save you, girl, come be in Shady's world.
+      (Ooh boy, you drive me crazy) Bitch, you make me hurl. First thing you
+      say: (I'm not fazed. I hang around big stars all day. I don't see what the
+      big deal is anyway. You're just plain old Marshall to me). Ooh yeah, girl,
+      run that game. (Hailie Jade, I love that name. Love that tattoo, what's
+      that say?. "Rot In Pieces," uh, that's great).
     </p>
   </q-page>
 </template>
@@ -58,5 +127,5 @@
 <script>
 export default {
   // name: 'PageName',
-}
+};
 </script>
