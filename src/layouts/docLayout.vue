2fd1f20cbeaf76a2e9c308d6bf55dbaf69ed74e5
MahmoodKhalil57 <2393401k@student.gla.ac.uk>
2022-05-01 15:51:03 +0100

    ba3basa neek

--- src/layouts/docLayout.vue
+++ src/layouts/docLayout.vue
@@ -7,7 +7,7 @@
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
           </q-avatar>
-          Title
+          Title 2
         </q-toolbar-title>
       </q-toolbar>
 
