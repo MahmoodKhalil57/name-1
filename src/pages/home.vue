b88e09fa301431f3688e85998c56e9ed5fc5172b
Omar Khorshid <okhorshid.00@gmail.com>
2022-06-14 21:56:34 +0200

    second section - home page (WIP)

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,6 +1,7 @@
 <template>
   <q-page class="text-center">
     <q-card>
+      <!--First Section-->
       <q-card-section>
         <q-toolbar class="q-py-lg">
           <q-card class="transparent no-shadow row justify-center">
@@ -50,67 +51,39 @@
           no-spinner
         />
       </q-card-section>
+      <!--Second Section-->
       <q-card-section class="bg-info q-pa-xl">
         <!-- content -->
-        <h1>About FarmWeb</h1>
+        <br />
+        <h2>But Why Mangoes</h2>
         <br />
 
         <p class="q-mx-xl text-left text-h6">
-          Cillum et deserunt esse labore in culpa exercitation ea laboris.
-          Adipisicing consectetur cillum adipisicing ullamco duis ea. Consequat
-          minim nulla magna aliqua aute. Dolor velit fugiat reprehenderit cillum
-          excepteur pariatur voluptate duis qui aliqua mollit tempor. Tempor
-          amet minim minim irure esse exercitation duis excepteur mollit
-          adipisicing. Laborum adipisicing pariatur proident nisi ut cillum. Non
-          Lorem ipsum minim officia nostrud Lorem cillum est proident eiusmod
-          elit. Minim nisi sint culpa in. Id consectetur minim tempor sint
-          aliqua eu minim qui dolor fugiat et excepteur in cupidatat.
-          Adipisicing enim et quis mollit esse. Nulla ea esse nisi tempor
-          nostrud ipsum incididunt ullamco labore dolore eiusmod consectetur
-          irure ad. Reprehenderit pariatur anim reprehenderit voluptate
-          reprehenderit minim. Aliquip esse ullamco ea aliquip qui irure quis
-          elit ut ex dolore dolor.
-          <br /><br />
-          Occaecat quis sit non ex ullamco officia. Non fugiat laboris aliqua
-          officia ea cillum cupidatat commodo id magna. Voluptate incididunt sit
-          sit cupidatat. Velit occaecat aute tempor Lorem officia nisi ut.
-          Ullamco qui voluptate sint nostrud officia excepteur mollit magna
-          Lorem in culpa non et. Voluptate dolore aute nostrud qui exercitation.
-          Eiusmod velit consequat ullamco adipisicing. Aute occaecat laboris
-          proident labore dolore et quis pariatur reprehenderit. Reprehenderit
-          Lorem in aliquip id. Nulla amet eiusmod minim aliquip et amet
-          voluptate labore consectetur. Eu eu elit id deserunt aliqua nostrud ex
-          cupidatat dolore nisi veniam cillum esse aute. Exercitation enim duis
-          sunt magna enim dolor quis ullamco. Occaecat enim fugiat sunt quis
-          dolor veniam esse exercitation esse ad cupidatat irure. Eiusmod fugiat
-          exercitation magna cillum ut qui dolor enim eu quis officia. Labore
-          minim ullamco consequat Lorem culpa et sit ex incididunt incididunt
-          mollit. Ipsum aliquip aute quis consectetur occaecat laboris eu.
-          Occaecat officia adipisicing do Lorem aute adipisicing consectetur non
-          voluptate proident nostrud aute. Occaecat velit velit qui qui quis
-          aliqua mollit ex. Sint irure aliquip qui dolore excepteur nisi.
-          <br /><br />
-          Officia pariatur elit laboris non reprehenderit nulla nulla id
-          reprehenderit. Id labore veniam magna do consectetur anim adipisicing
-          nostrud occaecat deserunt. Ut proident deserunt ex quis enim est
-          voluptate ullamco. Veniam exercitation est elit ut anim voluptate
-          adipisicing esse amet adipisicing officia aliquip. Deserunt dolor
-          pariatur ipsum sint occaecat sint magna duis qui tempor. Minim eu qui
-          nisi dolor amet cillum. Quis quis dolor sint elit deserunt est
-          pariatur in nostrud ut consequat magna amet. In quis fugiat occaecat
-          aliqua velit laboris cupidatat aute ullamco commodo culpa veniam
-          fugiat mollit. Aliqua excepteur veniam commodo esse do et irure. Lorem
-          et aute non in quis quis adipisicing dolor consectetur id voluptate
-          eiusmod in quis. Velit occaecat reprehenderit commodo ex voluptate
-          elit officia culpa incididunt ullamco eu tempor sit ex. Nostrud
-          consectetur non consequat sint cupidatat ipsum do. Aliqua mollit
-          voluptate tempor dolor sunt labore sit. Dolor qui occaecat consequat
-          proident nostrud quis anim occaecat esse. Ex exercitation deserunt
-          anim ut est ullamco deserunt. Nisi dolore nostrud do tempor id in
-          eiusmod labore consectetur pariatur magna ullamco culpa. Veniam est
-          velit eu velit aute aliqua eu quis Lorem pariatur.
+          A slice of FarmWeb's Mangoes fulfills 100% requirement of your daily
+          dose of Vitamin C. Besides, Mangoes are also rich in Vitamin A,
+          Vitamin B, and other antioxidants, which helps in keeping your immune
+          system strong, fights against cancerous cells, controls cholesterol,
+          and also cares and improves vision. [Copied] <br /><br />
         </p>
+
+        <div class="gridcontainer">
+          <div class="item1">
+            <q-icon class="fa-brands fa-envira" />
+            <h4>ALPHONSO</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+              repellendus accusamus vel hic iusto ab adipisci maiores neque
+              perferendis alias expedita saepe ex incidunt quos magnam qui
+              nesciunt aliquid dicta illo laudantium dolore, itaque provident
+              quisquam sint! Explicabo, totam porro!
+            </p>
+          </div>
+          <div class="item2"></div>
+          <div class="item3"></div>
+        </div>
+        <br /><br />
       </q-card-section>
+      <!--Third Section-->
       <q-card-section class="bg-green text-white">
         <!-- content -->
         <h1>About FarmWeb</h1>
@@ -172,6 +145,7 @@
           velit eu velit aute aliqua eu quis Lorem YABNI BA2A.
         </p>
       </q-card-section>
+      <!--Fourth Section-->
       <q-card-section class="bg-info">
         <!-- content -->
         <h1>About FarmWeb</h1>
