e7eedd643d0724f07974e4ba9cd5cdcfd104fbd4
Omar Khorshid <okhorshid.00@gmail.com>
2022-07-02 20:49:17 +0200

    sections 4, 5, 6 in progress. Co-authored-by: MahmoodKhalil

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -178,24 +178,31 @@
         <br /><br />
       </q-card-section>
       <!-- Fourth Section -->
-      <q-card-section class="q-pa-xl sectionContainer">
+      <q-card-section class="sectionContainer">
         <!-- content -->
         <div id="child1" class="q-mt-md">
           <br /><br />
           <h4 class="sectionHeader1">Our Core Values</h4>
         </div>
-        <div id="child2" class="gridContainer q-mt-xl">
+        <div
+          id="child2"
+          class="gridContainer q-mt-xl"
+          style="
+            max-width: 1300px;
+            grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+          "
+        >
           <!--item-1-->
           <div id="item1">
             <q-img
-              class="q-my-sm"
+              class="q-my-sm annoying-image-1"
               src="~assets\home\section_four.jpg"
               spinner-color="primary"
               spinner-size="82px"
             />
           </div>
           <!--item-2-->
-          <div id="item2" style="text-align: left">
+          <div id="item2" class="annoying-text-1">
             <h4 class="gridHeader1">
               IMPROVING AGRICULTURE, IMPROVING LIVES, CULTIVATING NEW CROPS
               THROUGH INNOVATION.
@@ -210,129 +217,82 @@
         </div>
         <br /><br />
       </q-card-section>
-      <!--Third Section-->
-      <q-card-section class="bg-green text-white">
+      <!--Fifth Section-->
+      <q-card-section class="text-white sectionContainer" style="z-index: 0">
         <!-- content -->
-        <h1>About FarmWeb</h1>
+        <h4 class="my-text" style="z-index: 1">
+          The Best Time To
+          <p class="text-yellow">Get Healthy</p>
+          is Now.
+        </h4>
         <br />
 
-        <p class="q-mx-xl text-left text-h6">
+        <p class="q-mx-xl text-left text-h6" style="z-index: 1">
           Cillum et deserunt esse labore in culpa exercitation ea laboris.
           Adipisicing consectetur cillum adipisicing ullamco duis ea. Consequat
           minim nulla magna aliqua aute. Dolor velit fugiat reprehenderit cillum
           excepteur pariatur voluptate duis qui aliqua mollit tempor. Tempor
           amet minim minim irure esse exercitation duis excepteur mollit
-          adipisicing. Laborum adipisicing pariatur proident nisi ut cillum. Non
-          Lorem ipsum minim officia nostrud Lorem cillum est proident eiusmod
-          elit. Minim nisi sint culpa in. Id consectetur minim tempor sint
-          aliqua eu minim qui dolor fugiat et excepteur in cupidatat.
-          Adipisicing enim et quis mollit esse. Nulla ea esse nisi tempor
-          nostrud ipsum incididunt ullamco labore dolore eiusmod consectetur
-          irure ad. Reprehenderit pariatur anim reprehenderit voluptate
-          reprehenderit minim. Aliquip esse ullamco ea aliquip qui irure quis
-          elit ut ex dolore dolor.
-          <br /><br />
-          Occaecat quis sit non ex ullamco officia. Non fugiat laboris aliqua
-          officia ea cillum cupidatat commodo id magna. Voluptate incididunt sit
-          sit cupidatat. Velit occaecat aute tempor Lorem officia nisi ut.
-          Ullamco qui voluptate sint nostrud officia excepteur mollit magna
-          Lorem in culpa non et. Voluptate dolore aute nostrud qui exercitation.
-          Eiusmod velit consequat ullamco adipisicing. Aute occaecat laboris
-          proident labore dolore et quis pariatur reprehenderit. Reprehenderit
-          Lorem in aliquip id. Nulla amet eiusmod minim aliquip et amet
-          voluptate labore consectetur. Eu eu elit id deserunt aliqua nostrud ex
-          cupidatat dolore nisi veniam cillum esse aute. Exercitation enim duis
-          sunt magna enim dolor quis ullamco. Occaecat enim fugiat sunt quis
-          dolor veniam esse exercitation esse ad cupidatat irure. Eiusmod fugiat
-          exercitation magna cillum ut qui dolor enim eu quis officia. Labore
-          minim ullamco consequat Lorem culpa et sit ex incididunt incididunt
-          mollit. Ipsum aliquip aute quis consectetur occaecat laboris eu.
-          Occaecat officia adipisicing do Lorem aute adipisicing consectetur non
-          voluptate proident nostrud aute. Occaecat velit velit qui qui quis
-          aliqua mollit ex. Sint irure aliquip qui dolore excepteur nisi.
-          <br /><br />
-          Officia pariatur elit laboris non reprehenderit nulla nulla id
-          reprehenderit. Id labore veniam magna do consectetur anim adipisicing
-          nostrud occaecat deserunt. Ut proident deserunt ex quis enim est
-          voluptate ullamco. Veniam exercitation est elit ut anim voluptate
-          adipisicing esse amet adipisicing officia aliquip. Deserunt dolor
-          pariatur ipsum sint occaecat sint magna duis qui tempor. Minim eu qui
-          nisi dolor amet cillum. Quis quis dolor sint elit deserunt est
-          pariatur in nostrud ut consequat magna amet. In quis fugiat occaecat
-          aliqua velit laboris cupidatat aute ullamco commodo culpa veniam
-          fugiat mollit. Aliqua excepteur veniam commodo esse do et irure. Lorem
-          et aute non in quis quis adipisicing dolor consectetur id voluptate
-          eiusmod in quis. Velit occaecat reprehenderit commodo ex voluptate
-          elit officia culpa incididunt ullamco eu tempor sit ex. Nostrud
-          consectetur non consequat sint cupidatat ipsum do. Aliqua mollit
-          voluptate tempor dolor sunt labore sit. Dolor qui occaecat consequat
-          proident nostrud quis anim occaecat esse. Ex exercitation deserunt
-          anim ut est ullamco deserunt. Nisi dolore nostrud do tempor id in
-          eiusmod labore consectetur pariatur magna ullamco culpa. Veniam est
-          velit eu velit aute aliqua eu quis Lorem YABNI BA2A.
+          adipisicing, YABNI BA2A.
         </p>
+        <q-img
+          class="q-my-sm header-image absolute-top"
+          src="~assets\home\section_5.jpg"
+          spinner-color="primary"
+          spinner-size="82px"
+          style="z-index: -1"
+        />
+        <div class="sectionContainer2 q-mx-md">
+          <q-btn>Shop now</q-btn>
+          <q-btn>Contact us</q-btn>
+        </div>
       </q-card-section>
       <!--Fourth Section-->
-      <q-card-section class="bg-info">
+      <q-card-section class="q-pa-xl sectionContainer">
         <!-- content -->
-        <h1>About FarmWeb</h1>
-        <br />
 
-        <p class="q-mx-xl text-left text-h6">
-          Cillum et deserunt esse labore in culpa exercitation ea laboris.
-          Adipisicing consectetur cillum adipisicing ullamco duis ea. Consequat
-          minim nulla magna aliqua aute. Dolor velit fugiat reprehenderit cillum
-          excepteur pariatur voluptate duis qui aliqua mollit tempor. Tempor
-          amet minim minim irure esse exercitation duis excepteur mollit
-          adipisicing. Laborum adipisicing pariatur proident nisi ut cillum. Non
-          Lorem ipsum minim officia nostrud Lorem cillum est proident eiusmod
-          elit. Minim nisi sint culpa in. Id consectetur minim tempor sint
-          aliqua eu minim qui dolor fugiat et excepteur in cupidatat.
-          Adipisicing enim et quis mollit esse. Nulla ea esse nisi tempor
-          nostrud ipsum incididunt ullamco labore dolore eiusmod consectetur
-          irure ad. Reprehenderit pariatur anim reprehenderit voluptate
-          reprehenderit minim. Aliquip esse ullamco ea aliquip qui irure quis
-          elit ut ex dolore dolor.
+        <div id="child1" class="q-mt-md">
           <br /><br />
-          Occaecat quis sit non ex ullamco officia. Non fugiat laboris aliqua
-          officia ea cillum cupidatat commodo id magna. Voluptate incididunt sit
-          sit cupidatat. Velit occaecat aute tempor Lorem officia nisi ut.
-          Ullamco qui voluptate sint nostrud officia excepteur mollit magna
-          Lorem in culpa non et. Voluptate dolore aute nostrud qui exercitation.
-          Eiusmod velit consequat ullamco adipisicing. Aute occaecat laboris
-          proident labore dolore et quis pariatur reprehenderit. Reprehenderit
-          Lorem in aliquip id. Nulla amet eiusmod minim aliquip et amet
-          voluptate labore consectetur. Eu eu elit id deserunt aliqua nostrud ex
-          cupidatat dolore nisi veniam cillum esse aute. Exercitation enim duis
-          sunt magna enim dolor quis ullamco. Occaecat enim fugiat sunt quis
-          dolor veniam esse exercitation esse ad cupidatat irure. Eiusmod fugiat
-          exercitation magna cillum ut qui dolor enim eu quis officia. Labore
-          minim ullamco consequat Lorem culpa et sit ex incididunt incididunt
-          mollit. Ipsum aliquip aute quis consectetur occaecat laboris eu.
-          Occaecat officia adipisicing do Lorem aute adipisicing consectetur non
-          voluptate proident nostrud aute. Occaecat velit velit qui qui quis
-          aliqua mollit ex. Sint irure aliquip qui dolore excepteur nisi.
-          <br /><br />
-          Officia pariatur elit laboris non reprehenderit nulla nulla id
-          reprehenderit. Id labore veniam magna do consectetur anim adipisicing
-          nostrud occaecat deserunt. Ut proident deserunt ex quis enim est
-          voluptate ullamco. Veniam exercitation est elit ut anim voluptate
-          adipisicing esse amet adipisicing officia aliquip. Deserunt dolor
-          pariatur ipsum sint occaecat sint magna duis qui tempor. Minim eu qui
-          nisi dolor amet cillum. Quis quis dolor sint elit deserunt est
-          pariatur in nostrud ut consequat magna amet. In quis fugiat occaecat
-          aliqua velit laboris cupidatat aute ullamco commodo culpa veniam
-          fugiat mollit. Aliqua excepteur veniam commodo esse do et irure. Lorem
-          et aute non in quis quis adipisicing dolor consectetur id voluptate
-          eiusmod in quis. Velit occaecat reprehenderit commodo ex voluptate
-          elit officia culpa incididunt ullamco eu tempor sit ex. Nostrud
-          consectetur non consequat sint cupidatat ipsum do. Aliqua mollit
-          voluptate tempor dolor sunt labore sit. Dolor qui occaecat consequat
-          proident nostrud quis anim occaecat esse. Ex exercitation deserunt
-          anim ut est ullamco deserunt. Nisi dolore nostrud do tempor id in
-          eiusmod labore consectetur pariatur magna ullamco culpa. Veniam est
-          velit eu velit aute aliqua eu quis Lorem YABNI BA2A.
-        </p>
+          <h4 class="sectionHeader1">Accreditation & Certifications</h4>
+
+          <ul
+            class="q-my-md text-h6 text-left sectionContent"
+            style="max-width: 1200px"
+          >
+            <li>
+              Registered in APEDA (Govt. of India) Horti-Net Farm Registration
+              Number: KA2901186103,101,102,104,202
+            </li>
+            <li>Certified under Global - Good Agricultural Practice (G.A.P)</li>
+            <li>
+              CERTIFICATE OF IMPORTER-EXPORTER CODE from Ministry of Commerce &
+              Industry - DGFT - Govt. Of Indian
+            </li>
+          </ul>
+        </div>
+        <div id="child2" class="gridContainer q-mt-xl">
+          <!--item-1-->
+          <div id="item1">
+            <q-img
+              src="~assets/certifications/apeda.svg"
+              style="height: 100px; width: 100px"
+            />
+          </div>
+          <!--item-2-->
+          <div id="item2">
+            <img
+              src="~assets/certifications/section_6_2.png"
+              style="height: 100px; object-fit: cover"
+            />
+          </div>
+          <!--item-3-->
+          <div id="item3">
+            <img
+              src="~assets/certifications/section_6_1.jpg"
+              style="height: 100px; object-fit: cover"
+            />
+          </div>
+        </div>
       </q-card-section>
     </q-card>
   </q-page>
@@ -354,5 +314,5 @@ export default {
   height: 105%
   opacity: 1
   filter: brightness(60%)
-  z-index: 1
+  z-index: 0
 </style>
