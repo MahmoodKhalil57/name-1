21655809c09d7c9e6321e1cf5406f0b7aedb9005
MahmoodKhalil57 <46604753+MahmoodKhalil57@users.noreply.github.com>
2022-07-14 15:33:39 +0100

    privacy policy page #22

--- src/pages/privacyPolicy.vue
+++ src/pages/privacyPolicy.vue
@@ -1,20 +1,26 @@
 <template>
   <q-page class="column items-center default-page" padding>
     <div class="default-header q-pb-xl">
-      <span class="default-header-text">Privacy Policy</span>
-    </div>
-    <div class="default-content column q-gutter-md">
-      <span
-        >Your privacy is important to us. We are committed to the SooperMango
-        Privacy Promise for Customers, which is as follows:</span
+      <span class="default-header-text"
+        >Refund, Return & Cancellation Policy</span
       >
-      <span
-        >While information is the cornerstone of our ability to provide superior
-        service, our most important asset is our customers trust. Keeping
-        customer information secure, and using it only as our customers would
-        want us to, is a top priority for all of us at SooperMango. Here then,
-        is our promise to our sooper customers:
-        <ul>
+    </div>
+    <div class="default-content column q-gutter-lg">
+      <div>
+        <span
+          >Your privacy is important to us. We are committed to the SooperMango
+          Privacy Promise for Customers, which is as follows:</span
+        >
+      </div>
+      <div>
+        <span
+          >While information is the cornerstone of our ability to provide
+          superior service, our most important asset is our customers trust.
+          Keeping customer information secure, and using it only as our
+          customers would want us to, is a top priority for all of us at
+          SooperMango. Here then, is our promise to our sooper customers:</span
+        >
+        <ul class="indented">
           <li>
             We will safeguard, according to strict standards of security and
             confidentiality, any information our customers share with us.
@@ -53,37 +59,89 @@
             how to notify us about errors which we will promptly correct.
           </li>
         </ul>
-      </span>
-      <span
-        >We collect the below information to provide better services to all of
-        our users:</span
-      >
-      <span
-        >Information you give us<br /><span class="indented"></span>For example,
-        many of our services require you to sign up for a SooperMango Account.
-        When you do, we'll ask for personal information, like your name, email
-        address, telephone number or credit card.</span
-      >
-      <span
-        >Information we get from your use of our services.<span class="indented"
-          >We may collect information about the services that you use and how
-          you use them such as:<br /><br /></span
-      ></span>
-      <span
-        >Information we get from your use of our services.<span
-          class="indented"
-        ></span
-      ></span>
-      <span
-        >Information we get from your use of our services.<span
-          class="indented"
-        ></span
-      ></span>
-      <span
-        >Information we get from your use of our services.<span
-          class="indented"
-        ></span
-      ></span>
+      </div>
+      <div>
+        <span
+          >We collect the below information to provide better services to all of
+          our users:</span
+        >
+      </div>
+      <div>
+        <span>Information you give us</span
+        ><span class="indented"
+          >For example, many of our services require you to sign up for a
+          SooperMango Account. When you do, we'll ask for personal information,
+          like your name, email address, telephone number or credit card.</span
+        >
+      </div>
+      <div>
+        <span>Information we get from your use of our services.</span>
+        <div class="indented">
+          <span
+            >We may collect information about the services that you use and how
+            you use them such as, <br /><br
+          /></span>
+          <span>
+            Device information<br />
+            We may collect device-specific information (such as your hardware
+            model, operating system version, unique device identifiers, and
+            mobile network information including phone number). SooperMango may
+            associate your device identifiers or phone number with your
+            SooperMango Account.</span
+          >
+          <span>
+            Log information<br />
+            When you use our services or view content provided by SooperMango,
+            we may automatically collect and store certain information in server
+            logs. This may include: Details of how you used our service, such as
+            your search queries,Internet p rotocol address. Device event
+            information such as crashes, system activity, hardware settings,
+            browser type, browser language, the date and time of your request
+            and referral URL. Cookies that may uniquely identify your browser or
+            your SooperMango Account.</span
+          >
+          <span>
+            Location information<br />
+            We may collect and process information about your actual location,
+            like GPS signals sent by a mobile device. We may also use various
+            technologies to determine location, such as sensor data from your
+            device that may, for example, provide information on nearby Wi-Fi
+            access points and cell towers.</span
+          >
+        </div>
+      </div>
+      <div>
+        <span>Cookies and anonymous identifiers</span
+        ><span class="indented">
+          We use various technologies to collect and store information when you
+          visit a SooperMango service, and this may include sending one or more
+          cookies or anonymous identifiers to your device. Third party vendors,
+          including Google, use cookies to serve ads based on your prior visits
+          to our website. Google's use of the DoubleClick cookie enables it and
+          its partners to serve ads based on your visit to our site and/or other
+          sites on the Internet. You may opt out of the use of the DoubleClick
+          cookie for interest-based advertising by visiting Ads Settings.
+          (Alternatively, you can direct users to opt out of a third-party
+          vendor's use of cookies for interest based advertising by visiting
+          aboutads.info.) If you have not opted out of third-party ad serving,
+          the cookies of other third-party vendors or ad networks may also be
+          used to serve ads on our site.
+        </span>
+      </div>
+      <div>
+        <span> How we use information we collect</span
+        ><span class="indented">
+          We use the information we collect from all of our services to provide,
+          maintain, protect and improve them, to develop new ones, and to
+          protect SooperMango and our customers. We also use this information to
+          offer you tailored content – like giving you more relevant search
+          results and features. When you contact SooperMango, we may keep a
+          record of your communication to help solve any issues you might be
+          facing. We may use your email address to inform you about our services
+          / products, such as letting you know about upcoming changes or
+          improvements.</span
+        >
+      </div>
     </div>
   </q-page>
 </template>
@@ -114,6 +172,7 @@ export default {
   padding-left: 200px;
 }
 .indented {
+  display: block;
   padding-left: 50px;
 }
 </style>
