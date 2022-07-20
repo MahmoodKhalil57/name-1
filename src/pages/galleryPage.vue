250c58b9eff8c4131ba46873489a3864ede3adf9
MahmoodKhalil57 <46604753+MahmoodKhalil57@users.noreply.github.com>
2022-07-20 23:04:16 +0100

    Add gallery page that works with google album #29

--- src/pages/galleryPage.vue
+++ src/pages/galleryPage.vue
@@ -4,23 +4,24 @@
     <!--Item1:header-->
     <span class="header-title text-center q-mt-xl q-mb-lg"> Gallery </span>
     <!--Item2:FlexRowContainer-->
-    <div class="row q-pa-xl q-mb-xl q-gutter-lg justify-between flex-container">
+    <div class="row q-pa-xl q-gutter-lg justify-between flex-container">
       <!--5Items Of Item2: FlexColumnContainer-->
-      <div class="flex-item" v-for="image in images" :key="image">
+      <div class="column flex-item" v-for="image in images" :key="image">
         <img
           v-if="!!image.image_link"
           :src="image.image_link"
-          style="width: 20vw; height: 20vw; content: ''; max-width: 300px"
+          class="defualt-width default-height"
         />
         <iframe
-          class="floating-video"
+          v-else
+          class="floating-video defualt-width default-height"
           :src="image.video_link"
           title="SooperMango - Driving straight into a tropical mango haven!"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
         ></iframe>
-        <div class="text-center">
+        <div class="q-pa-sm content-text defualt-width default-height">
           <span>{{ image.content }} </span>
         </div>
       </div>
@@ -36,7 +37,8 @@ export default {
       images: [
         {
           video_link: "https://www.youtube.com/embed/_CpEdlOZLvk",
-          content: "Orchard View",
+          content:
+            "Leave the city buzz behind, and drive straight into a tropical mango haven!",
         },
         {
           image_link: "https://soopermango.com/images/gallery-images/1.jpeg",
@@ -44,23 +46,48 @@ export default {
         },
         {
           image_link: "https://soopermango.com/images/gallery-images/2.jpeg",
-          content: "Orchard View",
-        },
-        {
-          image_link: "https://soopermango.com/images/gallery-images/3.jpeg",
           content: "In the midst of Rajgira Mango Tree",
         },
         {
+          image_link: "https://soopermango.com/images/gallery-images/3.jpeg",
+          content: "Hand Picked and stored in container",
+        },
+        {
+          image_link: "https://soopermango.com/images/gallery-images/6.jpeg",
+          content: "Safely transported to ripening chamber",
+        },
+        {
           image_link: "https://soopermango.com/images/gallery-images/4.jpeg",
-          content: "Hand Picked and stored in container",
+          content: "Mangoes are sorted manually and put inside the 3kg box",
         },
         {
           image_link: "https://soopermango.com/images/gallery-images/5.jpeg",
-          content: "Hand Picked and stored in container",
+          content: "Ready to be delivered",
         },
         {
-          image_link: "https://soopermango.com/images/gallery-images/5.jpeg",
-          content: "Hand Picked and stored in container",
+          image_link:
+            "https://soopermango.com/images/gallery-images/IMG_2252.jpg",
+          content: "Hill View",
+        },
+        {
+          image_link:
+            "https://soopermango.com/images/gallery-images/IMG_2253.jpg",
+          content: "Mangoes",
+        },
+        {
+          image_link:
+            "https://soopermango.com/images/gallery-images/IMG_2255.jpg",
+          content: "Mallika",
+        },
+        {
+          image_link:
+            "https://soopermango.com/images/gallery-images/IMG_2367.jpg",
+          content: "Mallika - Mango season starts!",
+        },
+        {
+          image_link:
+            "https://soopermango.com/images/gallery-images/harvesting_totapuri.jpg",
+          content: "Totapuri Harvesting & Packing",
         },
       ],
     };
@@ -69,36 +96,24 @@ export default {
 </script>
 
 <style lang="scss">
-.flex-item {
-  border-style: solid;
-  border-width: 1px;
-  max-width: 300px;
-  height: 100%;
-}
-
 .flex-container {
-  border-style: solid;
-  border-width: 1px;
   max-width: 1200px;
-}
-.title-fonts {
-  font: normal normal 900 22px/42px Thasadith, sans-serif;
-  color: #323648;
-  text-transform: uppercase;
-}
-.content-fonts {
-  font: normal normal 100 17px/34px Verdana;
-  color: #707579;
-}
-
-.id-fonts {
-  font: normal normal 700 24px/36px Roboto, sans-serif;
-  color: #009f4d;
 }
 
 .header-title {
   font: normal normal 700 30px/36px Thasadith, sans-serif;
   color: #323648;
+}
+.content-text {
+  font: normal normal 100 17px/34px "Verdana", sans-serif;
+  color: #707579;
+}
+.default-height {
+  width: 20vw;
+  max-width: 300px;
+}
+.default-width {
+  height: auto;
 }
 </style>
 }
