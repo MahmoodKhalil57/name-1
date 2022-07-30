0156e09c32e6b744bbb8a0074498188224404bd3
MahmoodKhalil57 <46604753+MahmoodKhalil57@users.noreply.github.com>
2022-07-30 21:01:22 +0100

    :/

--- src/store/mydata/actions.js
+++ src/store/mydata/actions.js
@@ -3,6 +3,7 @@ import { api } from "boot/axios";
 export async function updateProductsFromDatabase(state) {
   if(!state.productData) {
     await api.get('/products').then(result => {
+      console.log("result: ",result);
       state.commit("setProducts", result.data);
     });
   }
